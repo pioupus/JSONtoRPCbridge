1da20cac16a99ab00d54d3dca1bde9f4194ef990
arne <raspi@example.com>
2018-03-29 12:04:36 +0000

    performance improvement of python proof of concept binding

--- python/pyRPCProtocol.py
+++ python/pyRPCProtocol.py
@@ -3,30 +3,33 @@ import subprocess
 import time
 import os
 import json
-import collections
+
 
 
 class RPCProtocol:
     def __init__(self, comport_path, baud, xml_search_dir):
         my_env = os.environ.copy()
-        my_env["LD_LIBRARY_PATH"] = "../build-json_to_rpc_bridge-Desktop-Debug/bin/debug"
+        my_env["LD_LIBRARY_PATH"] = "/home/pi/JSONtoRPCbridge/bin/release"
 
-        self.rpc_json_bridge_process = subprocess.Popen(["../build-json_to_rpc_bridge-Desktop-Debug/bin/debug/consoleapp", comport_path, str(baud), xml_search_dir], env=my_env, cwd=r'../', stdin=subprocess.PIPE,stdout=subprocess.PIPE, shell = False)
+        self.rpc_json_bridge_process = subprocess.Popen(["/home/pi/JSONtoRPCbridge/bin/release/consoleapp", comport_path, str(baud), xml_search_dir], env=my_env, cwd=r'../', stdin=subprocess.PIPE,stdout=subprocess.PIPE, shell = False)
+        self._start_tag_positions = []
+        self._stop_tag_positions = []
+
+
+
 
     def _test_json_input(self, test_str):
         result = True
         test_str = test_str[2:-2];                # remove the / of the /{
         
-#        print("test_str: "+test_str)
+        #print("test_str: "+test_str)
         decoded_answer = {}
         try:
             decoded_answer = json.loads(test_str)
         except ValueError:
             result = False
         
-        rpc = collections.namedtuple('RPC', ['answer', 'success'])
-        rpc.success = result
-        rpc.answer = decoded_answer
+        rpc = {"success": result, "answer":decoded_answer}
         
         return rpc
     
@@ -38,69 +41,91 @@ class RPCProtocol:
         input_not_empty = True
         finished = False
         decoded_answer = {}
+        count = 0;
+        duration_position_time = 0;
+        duration_stop_tag_time = 0;
+        duration_start_tag_time = 0;
+
         while input_not_empty:
-            start_tag_positions = []
-            stop_tag_positions = []
+
             found = False
-            i = -1
-            while True:
-                i = self._input_buffer.find("/{", i + 1)
-                if i > -1:
-                    start_tag_positions.append(i)
-                
-                if i < 0:
-                    break
+            
+            if self._input_buffer.endswith("/{"):
+                self._start_tag_positions.append(len(self._input_buffer)-2);
+            
 
-            i = -1
-            while True:
-                i = self._input_buffer.find("}/", i + 1);
-                if i > -1:
-                    stop_tag_positions.append(i);
-                
-                if i < 0:
-                    break
+            if self._input_buffer.endswith("}/"):
+                self._stop_tag_positions.append(len(self._input_buffer)-2);
 
-            for start in start_tag_positions:
-                for stop in stop_tag_positions:
-                    if stop < start:
-                        continue
-                    
-                    test_str = self._input_buffer[start : stop  + len('}/')]
-                    #print("test_str"+test_str)
-                    rpc_result = self._test_json_input(test_str)
-                    if rpc_result.success:
-                        self._input_buffer = self._input_buffer[stop +len("}/"):];
-                        finished = True;
-                        found = True;
-                        decoded_answer = rpc_result.answer
-                        break;
+            positions_tag_time = time.clock()
+            if self._input_buffer.endswith("}/"):
+                for start in self._start_tag_positions:
 
+                    for stop in self._stop_tag_positions:
+                        if stop < start:
+                            continue
+                        test_str = self._input_buffer[start : stop  + len('}/')]
+                        rpc_result = self._test_json_input(test_str)
+                        if rpc_result["success"]:
+                            self._input_buffer = self._input_buffer[stop +len("}/"):];
+                            finished = True;
+                            found = True;
+                            decoded_answer = rpc_result["answer"]
+                            break;
+            
             if found == False:
                 input_not_empty = False
-                
-        rpc = collections.namedtuple('RPC', ['answer', 'finished'])
-        rpc.finished = finished
-        rpc.answer = decoded_answer
+
+        rpc = {"finished": finished, "answer":decoded_answer}
         return rpc
         
 
     def _send_and_receive_json(self,json_request, await_answer = True):
+        start = time.clock()
         self._input_buffer = ''
+        self._start_tag_positions = []
+        self._stop_tag_positions = []
+        
         rpc_request_string = json.dumps(json_request)
-        #print("rpc_request_string: "+ rpc_request_string)
+        print("rpc_request_string: "+ rpc_request_string)
 
+        #start_stdinwrite = time.clock()
         self.rpc_json_bridge_process.stdin.write('/'+rpc_request_string+'/\n')
         self.rpc_json_bridge_process.stdin.flush()
+        #print("stdinwrite[ms]: "+str((time.clock() - start_stdinwrite)*1000))
 
-
-
+        byte_count = 0
+        duration_total = 0.0
+        duration_total_read = 0.0
+        duration_total_append = 0.0
         while await_answer:
+            byte_count = byte_count +1
+            
+           # start_time = time.clock()
             answer_byte = self.rpc_json_bridge_process.stdout.read(1);
+          #  duration_read = (time.clock() - start_time)*1000
+            
+            #start_time = time.clock()
             rpc_answer = self._append_answer_byte(answer_byte)
-            if rpc_answer.finished:
-                return rpc_answer.answer
+           # duration_append = (time.clock() - start_time)*1000
             
+            ##duration_total = duration_total + duration_append + duration_read
+            #duration_total_append = duration_total_append + duration_append
+            #duration_total_read = duration_total_read + duration_read
             
+  
+            if rpc_answer["finished"]:
+               # print("count: "+str(byte_count)+
+                #      " total: "+str(duration_total)+
+                #      " duration_total_append: "+str(duration_total_append)+
+                #      " duration_total_append_avg: "+str(duration_total_append/byte_count)+
+                #      " duration_total_read: "+str(duration_total_read)
+               #       )
+                #print("_send_and_receive_json[ms]: "+str((time.clock() - start)*1000))
+                return rpc_answer["answer"]
+            #time.sleep(0.05)
+            
+        
             
     def __del__(self):
         rpc_request = {
@@ -148,22 +173,22 @@ class RPCProtocol:
             }
         decoded_answer = self._send_and_receive_json(rpc_request)
         
-        version_info = collections.namedtuple('version_info', ['git_hash', 'git_unix_date', 'git_string_date'])
-
-        version_info.git_hash = decoded_answer['controll']['result']['git_hash']
-        version_info.git_unix_date = decoded_answer['controll']['result']['git_unix_date']
-        version_info.git_string_date = decoded_answer['controll']['result']['git_string_date']
+        version_info =   { "git_hash": decoded_answer['controll']['result']['git_hash'],
+                   "git_unix_date":decoded_answer['controll']['result']['git_unix_date'],
+                   "git_string_date":decoded_answer['controll']['result']['git_string_date']
+                }
+        
         return version_info
                     
     
         
 
-proto = RPCProtocol("/dev/ttyUSB0",115200,"/home/arne/programming/projekte/crystalTestFramework_data/xml/")
+proto = RPCProtocol("/dev/ttyS0",115200,"/home/pi/thf_logger/xml_rpc/")
 
 #time.sleep(0.1)
 arguments_get_adc_values = {}
 print("hash: "+proto.get_server_hash())
-print("git: "+str(proto.get_version().git_hash))
+print("JSON Bridge git: "+str(proto.get_version()["git_hash"]))
 
 result = proto.call("get_adc_values",arguments_get_adc_values)
 print("rpc_result: "+str(result))
@@ -174,13 +199,18 @@ arguments_get_adc_values = {}
 result = proto.call("get_device_descriptor",arguments_get_adc_values)
 print("rpc_result: "+str(result))
 
-test_function_param = {"param8":230,"param32":324234}
-result = proto.call("test_function_param",test_function_param)
-print(result)      
+while 1:
+    test_function_param = {"channel":3}
+    start_time = time.clock()
+    result = proto.call("get_sample_data",test_function_param)
+    duration = (time.clock() - start_time)*1000
+    print("duration[ms]: "+str(duration))
+    print(result)      
+    time.sleep(0.5)
   
-test_function_struct = {"param8_inout":{"running_number":1212,"name":"hallo","version":"vers"},"param32_inout":{"running_number":1212,"name":"hallo","version":"vers"}}
-result = proto.call("test_function_struct",test_function_struct)
-print("test_function_struct: "+str(result))
+#test_function_struct = {"param8_inout":{"running_number":1212,"name":"hallo","version":"vers"},"param32_inout":#{"running_number":1212,"name":"hallo","version":"vers"}}
+#result = proto.call("test_function_struct",test_function_struct)
+#print("test_function_struct: "+str(result))
 
 
 
